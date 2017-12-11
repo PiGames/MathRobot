d6e01bf90b27f1837d34fd56109226c71d38897b
Bartosz Legięć <bartosz@legiec.eu>
2017-12-11 23:01:41 +0100

    Removed msg from result

--- src/Logs/DoneStep.jsx
+++ src/Logs/DoneStep.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {CardMedia, CardTitle} from 'material-ui/Card'
+import {CardMedia} from 'material-ui/Card'
 
 const DoneStep = ( { step } ) => {
-  const { msg, img } = step
+  const { img } = step
 
   return (
-    <CardMedia overlay={<CardTitle title={ msg } />} className="col-sm-12 col-lg-8 mx-auto">
+    <CardMedia className="col-sm-12 col-lg-8 mx-auto">
       <img src={ img } alt="" />
     </CardMedia>
   )
